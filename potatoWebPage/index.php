87ef79b4c21a063e35b20f552025dd7632ffe646
Nismal <40026531+Nismal@users.noreply.github.com>
2019-02-26 20:25:42 +0100

    Añadido BaseXUtils

--- potatoWebPage/index.php
+++ potatoWebPage/index.php
@@ -4,4 +4,10 @@
     require('views/body.php');
 
     require('views/footer.php');
+
+	require('BaseXUtils.php');	
+	
+	BaseXUtils::StartSesssion();
+
+	echo BaseXUtils::ExecuteQuery('for $name in collection("C:\xampp\htdocs\Rotten_Potatoes\BaseX912\basex\test.xml")/tests/test where $name[matches($name, \'Luis\')]return $name');
 ?>

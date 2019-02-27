84573d423eaf3d19fbed13981981f4634eb11642
Nismal <ismaelperez1299@gmail.com>
2019-02-28 00:17:31 +0100

    subdividiod el body en noticias y peliculas

--- potatoWebPage/views/body.php
+++ potatoWebPage/views/body.php
@@ -1,16 +1,11 @@
+
+
+
 <div class="articles">
-    <div class="noticiasRecientes">
-        <h2>Noticias Recientes</h2>
-        <div class="contenidoCentral">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, veritatis.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, quibusdam tempore. Provident tenetur ipsa perspiciatis praesentium explicabo fugiat vel accusamus!</p>
-        </div>
-    </div>
-    <div class="peliculasRecientes">
-        <h2>Películas Recientes</h2>
-        <div class="contenidoCentral">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat?</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vero, inventore, amet at earum architecto iusto alias quidem repellat perspiciatis accusamus. Blanditiis voluptas quod quos mollitia quia rem harum ipsum.</p>
-        </div>
-    </div>
+    <?php
+        require('noticias.view.php');
+        require('peliculas.view.php');
+    
+    ?>
+
 </div>

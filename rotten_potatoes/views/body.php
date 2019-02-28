bf82651d67eff8b00e23ebdc22f61db8d369a15f
MrLapina <46464199+MrLapina@users.noreply.github.com>
2019-02-28 20:01:10 +0100

    .

--- rotten_potatoes/views/body.php
+++ rotten_potatoes/views/body.php
@@ -4,7 +4,6 @@
 <div class="articles">
     <?php
         require('noticias.view.php');
-        require('peliculas.view.php');
     
     ?>
 
--- rotten_potatoes/views/peliculas.view.php
+++
@@ -1,7 +0,0 @@
-    <div class="peliculasRecientes">
-        <h2>Películas Recientes</h2>
-        <div class="contenidoCentral">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat?</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vero, inventore, amet at earum architecto iusto alias quidem repellat perspiciatis accusamus. Blanditiis voluptas quod quos mollitia quia rem harum ipsum.</p>
-        </div>
-    </div>

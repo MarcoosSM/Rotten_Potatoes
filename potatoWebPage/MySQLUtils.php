2ef19ce64cc548f397191f3c922d15b25bce65b4
MrLapina <46464199+MrLapina@users.noreply.github.com>
2019-02-28 19:21:51 +0100

    he hecho cosas chuchis

---
+++ potatoWebPage/MySQLUtils.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class MySQLUtils{
+    
+    static private $session;
+
+    
+    static function getNew(){
+        $statement = self::$session->prepare('SELECT titulo,noticia FROM noticias');
+        $statement->execute();
+        $resultado = $statement->fetchAll();
+        foreach ($resultado as &$noticia){
+            echo $noticia[0].$noticia[1];
+        }
+            
+    }
+    
+    static function StartSession() {
+		self::$session = new PDO('mysql:host=192.168.3.50;dbname=rottenpotatoes_db', 'invitado', '1234');
+
+	}
+	
+	static function CloseSesssion() {		
+		self::$session->close();
+	}
+    
+}
+
+
+?>

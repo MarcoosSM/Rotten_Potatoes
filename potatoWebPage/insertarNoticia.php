c621be13357f2806621c642b233c480a3090f82c
Nismal <40026531+Nismal@users.noreply.github.com>
2019-02-28 18:35:36 +0100

    test

---
+++ potatoWebPage/insertarNoticia.php
@@ -0,0 +1,11 @@
+<?php
+	require BaseXUtils.php;
+
+	$title = $_POST["title"];
+	$content = $_POST["content"];
+
+	#imodificar en la base de datos 
+
+	header("index.php");
+				
+?>
---
+++ potatoWebPage/modificarNoticia.php
@@ -0,0 +1,12 @@
+<?php
+	require BaseXUtils.php;
+
+	$title = $_POST["title"];
+	$content = $_POST["content"];
+	$id = $_POST["id"];
+	#imodificar en la base de datos 
+
+	header("index.php");
+		
+		
+?>

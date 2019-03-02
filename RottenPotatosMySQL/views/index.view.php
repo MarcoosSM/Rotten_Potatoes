9cfad64c2f83494997d2c3a78e948a7f4ff80331
Nismal <ismaelperez1299@gmail.com>
2019-03-02 21:41:59 +0100

    Update index.view.php

--- RottenPotatosMySQL/views/index.view.php
+++ RottenPotatosMySQL/views/index.view.php
@@ -18,8 +18,7 @@
                     <button type="submit"><i class="fa fa-search"></i></button>
                 </form>
             </div>
-            <a class="active" href="#home">Home</a>
-            <a href="#news">Noticias</a>
+            <a class="active" href="php_login/cerrarSesion.php">Cerrar Sesion</a>
         </div>
     </div>
 

08a8cd7a8a2c41e8d3046ff3e034bb49a437815a
MrLapina <46464199+MrLapina@users.noreply.github.com>
2019-03-02 17:10:39 +0100

    .

--- rotten_potatoes/views/body.view.php
+++
@@ -1,9 +0,0 @@
-<div class="articles">
-    <div class="noticiasRecientes">
-        <h2>Noticias Recientes</h2>
-        <div class="contenidoCentral">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, veritatis.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, quibusdam tempore. Provident tenetur ipsa perspiciatis praesentium explicabo fugiat vel accusamus!</p>
-        </div>
-    </div>
-</div>

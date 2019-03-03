7909e2adee7eeb504aaadc0b7a353125f9db0a64
Nismal <ismaelperez1299@gmail.com>
2019-03-03 18:54:12 +0100

    asd

---
+++ RottenPotatosXML/BaseXUtils.php
@@ -0,0 +1,33 @@
+<?php
+require ('BaseXClient.php');
+class BaseXUtils{
+
+	static private $session;
+	
+    
+    static function insertUser($usermane, $password){
+        self::$session->query('insert node <username>'.$username.'</username> into /bbdd/users');#Inserta en usuario
+        self::$session->query('insert node <password>'.password.'</password> into /bbdd/users');#Inserta en contraseña
+    }
+    
+    static function getNews(){
+        return self::$session->query('for $new in doc(\'../potatoWebPage/bbdd.xml\')//bbdd//news//new return $new')
+    }
+    
+    
+	static function ExecuteQuery($queryStr) {
+		echo "test2";
+		$query = self::$session->query($queryStr);
+		return $query->execute();
+		
+	}
+	
+	static function StartSesssion() {
+		self::$session = new Session("192.168.3.125", 1984, "admin", "admin");
+	}
+	
+	static function CloseSesssion() {		
+		self::$session->close();
+	}
+}
+?>

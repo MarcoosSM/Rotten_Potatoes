7fd962cc18d3eebc2e9f10d2106ccbf444e8b0ea
MiguelAngelDAM <44416407+MiguelAngelDAM@users.noreply.github.com>
2019-03-03 20:31:35 +0100

    Actualizados Estilos

--- RottenPotatosMySQL/php_noticias/modificarNoticia.php
+++ RottenPotatosMySQL/php_noticias/modificarNoticia.php
@@ -18,17 +18,17 @@ if(!empty($_POST)){
             if($_POST[$noticia[0]] == "Modificar"){
                 
                 echo '<form action="'. htmlspecialchars($_SERVER["PHP_SELF"]) .'" method="POST">';
-                echo '<input type="text" value="'.$noticia[0].'" name="id" style="display:none;"/>';
-                echo '<input type="text" name="titulo" value="'.$noticia[1].'"/>';
-                echo '<textarea row="4" cols="50" name="noticia">'.$noticia[2].'</textarea>';
-                echo '<input type=submit value="Enviar" name="Modificar"/>';
+                echo '<input type="text" class="textModifyArticles" value="'.$noticia[0].'" name="id" disabled/>';
+                echo '<input type="text" class="textModifyArticles" name="titulo" value="'.$noticia[1].'"/>';
+                echo '<textarea class="textModifyArticles" row="4" cols="50" name="noticia">'.$noticia[2].'</textarea>';
+                echo '<input type=submit class ="buttonsModifyArticles" value="Enviar" name="Modificar"/>';
                 echo '</form>';
                 
             }elseif($_POST[$noticia[0]] == "Eliminar"){
                 echo '<form action="'. htmlspecialchars($_SERVER["PHP_SELF"]) .'" method="POST">';
-                echo '<input type="text" value="'.$noticia[0].'" name="id" style="display:none;"/>';
+                echo '<input type="text" class="textModifyArticles" value="'.$noticia[0].'" name="id" disabled/>';
                 echo '<label>'.$noticia[1].'</label>';
-                echo '<input type=submit value="Enviar" name="Eliminar"/>';
+                echo '<input type=submit class ="buttonsModifyArticles" value="Enviar" name="Eliminar"/>';
                 echo '</form>';
             }
         }
